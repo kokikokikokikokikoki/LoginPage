84346f9be9158c346bdce66134b5bc1e307c47d9
Benjamard Meeboon <benjamard.m@cimbthai.com>
2022-02-08 18:10:05 +0700

    Refactor validation logic for username, password

--- LoginPage/ViewController.swift
+++ LoginPage/ViewController.swift
@@ -7,8 +7,8 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextViewDelegate, UITextFieldDelegate {
-
+class ViewController: UIViewController {
+    
     @IBOutlet weak var userEmail: UITextField!
     @IBOutlet weak var passwordText: UITextField!
     @IBOutlet weak var loginButton: UIButton!
@@ -19,151 +19,80 @@ class ViewController: UIViewController, UITextViewDelegate, UITextFieldDelegate 
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        userEmail.delegate = self
+        passwordText.delegate = self
         resetForm()
-        //userEmail.layer.cornerRadius = 20
-        
-//        userEmail.delegate = self
-//        passwordText.delegate = self
-//        labelMessage.isHidden = true
-        
     }
-    func resetForm()
-    {
-        loginButton.isEnabled = false
+
+    func resetForm() {
         labelMessage.isHidden = true
-//        passwordError.isHidden = true
-        
+        passwordError.isHidden = true
         labelMessage.text = "Required"
-//        passwordError.text = "Required"
-        
         userEmail.text = ""
-       // passwordText.text = ""
-        
     }
-    @IBAction func emailChanged(_ sender: Any) {
-        
-        if let email = userEmail.text
-        {
-            if let errorMessage = invalidEmail(value: email)
-            {
-                labelMessage.text = errorMessage
-                labelMessage.isHidden = false
-            }
-            else{
-                labelMessage.isHidden = true
-            }
-        }
-        checkValidForm()
-    }
-    func invalidEmail(  value: String)-> String?{
+
+    func isValidEmail(email: String) -> Bool {
         let regularExpression = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}"
         let predicate = NSPredicate(format: "SELF MATCHES %@", regularExpression )
-        if !predicate.evaluate(with: value)
-        {
-            return "Invalid Email"
-        }
-        return nil
-        
+        return predicate.evaluate(with: email)
     }
-    
-    @IBAction func passwordChanged(_ sender: Any) {
-        
-        checkValidForm()
-        
-    }
-    
-func checkValidForm()
-    {
-        if labelMessage.isHidden
-        {
-            loginButton.isEnabled = true
-        }else{
-            loginButton.isEnabled = false
-        }
-    }
-    
+
     @IBAction func loginTap(_ sender: Any) {
-        
-//        let email = userEmail.text
-        
-        
-        resetForm()
-        
-        
-        
+        if userEmail.text?.isEmpty ?? false {
+            labelMessage.text = "Required"
+            labelMessage.isHidden = false
+            return
+        }
+        else if !isValidEmail(email: userEmail.text ?? "") {
+            labelMessage.text = "Invalid email"
+            labelMessage.isHidden = false
+            return
+        }
+        labelMessage.isHidden = true
+        redirectToWelcomeScreen()
+    }
 
-    
-    
-    
-    
+    private func redirectToWelcomeScreen() {
+        print("redirectToWelcomeScreen")
+    }
+
+    private func validateUsername() {
+        if userEmail.text?.isEmpty ?? false {
+            labelMessage.text = "Required"
+            labelMessage.isHidden = false
+        }
+        else if !isValidEmail(email: userEmail.text ?? "") {
+            labelMessage.text = "Invalid email"
+            labelMessage.isHidden = false
+        } else {
+            labelMessage.isHidden = true
+        }
+    }
+
+    private func validatePassword() {
+        if passwordText.text?.isEmpty ?? false {
+            passwordError.text = "Required"
+            passwordError.isHidden = false
+        }
+        else if passwordText.text?.count ?? 0 < 6 {
+            passwordError.text = "Required more than 6 charactor"
+            passwordError.isHidden = false
+        } else {
+            passwordError.isHidden = true
+        }
+    }
 }
 
-//    @IBAction func emailChanged(_ sender: Any) {
-//
-//        if let email = userEmail.text
-//        {
-//            if let errorMessage = invalidEmail(value: email)
-//            {
-//                labelMessage.text = errorMessage
-//                labelMessage.isHidden = false
-//            }
-//            else{
-//                labelMessage.isHidden = true
-//            }
-//        }
-//        checkValidForm()
-//    }
-//    func invalidEmail(  value: String)-> String?{
-//        let regularExpression = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
-//        let predicate = NSPredicate(format: "SELF MATCHES %@", regularExpression )
-//        if !predicate.evaluate(with: value)
-//        {
-//            return "Invalid Email"
-//        }
-//        return nil
-//
-//    }
-//
+extension ViewController: UITextFieldDelegate {
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        validateUsername()
+        validatePassword()
+        return true
+    }
     
-    
-//    @IBAction func passwordError(_ sender: Any) {
-//
-//        if let password = passwordText.text
-//        {
-//            if let errorMessage = invalidPassword(value: password)
-//            {
-//                passwordError.text = errorMessage
-//                passwordError.isHidden = false
-//            }
-//            else{
-//                passwordError.isHidden = true
-//            }
-//        }
-//        checkValidForm()
-//
-//    }
-//
-//    func invalidPassword(  value: String)-> String?{
-//
-//        if value.count < 8
-//        {
-//            return "Password must be atleast 8 char"
-//        }
-//        if containsDigits(value: value)
-//        {
-//            return "password must containt digits"
-//     }
-//        return nil
-//
-//    }
-//    func containsDigits(value: String)-> Bool
-//    {
-//
-//        let regularExpression = ".*[0-9].*"
-//        let predicate = NSPredicate(format: "SELF MATCHES %@", regularExpression )
-//        return !predicate.evaluate(with: value)
-//
-//    }
-    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+    }
 }
+
+
 
